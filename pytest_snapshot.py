39847e819500d9bf1b7b36f3245b7b21d3481bfe
Joseph Roitman <joseph.roitman@gmail.com>
2020-03-18 02:39:20 +0200

    Add support for --snapshot-update.

--- pytest_snapshot.py
+++ pytest_snapshot.py
@@ -2,7 +2,7 @@
 
 import pytest
 from pathlib2 import Path
-from typing import Optional
+from typing import Optional, List
 
 
 def pytest_addoption(parser):
@@ -15,16 +15,38 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def snapshot(request):
-    return Snapshot(request.config.option.snapshot_update)
+    with Snapshot(request.config.option.snapshot_update) as snapshot:
+        yield snapshot
 
 
 class Snapshot(object):
 
+    _created_snapshots = None  # type: List[str]
+    _updated_snapshots = None  # type: List[str]
     _snapshot_update = None  # type: bool
     _snapshot_dir = None  # type: Optional[Path]
 
     def __init__(self, snapshot_update):
+        self._created_snapshots = []
+        self._updated_snapshots = []
         self._snapshot_update = snapshot_update
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            return False
+        if self._created_snapshots or self._updated_snapshots:
+            message_lines = []
+            if self._created_snapshots:
+                message_lines.append("The following snapshots were created in '{}':".format(self._snapshot_dir))
+                message_lines.extend('  ' + s for s in self._created_snapshots)
+
+            if self._updated_snapshots:
+                message_lines.append("The following snapshots were updated in '{}':".format(self._snapshot_dir))
+                message_lines.extend('  ' + s for s in self._updated_snapshots)
+            raise AssertionError('\n'.join(message_lines))
 
     @property
     def snapshot_dir(self):
@@ -49,12 +71,18 @@ class Snapshot(object):
         :type value: str
         :type snapshot_name: str
         """
-        if self._snapshot_update:
-            raise NotImplementedError
-
         snapshot_path = self._snapshot_path(snapshot_name)
-        if snapshot_path.exists():
-            expected_value = snapshot_path.read_text()
-            assert expected_value == value
+        if self._snapshot_update:
+            if snapshot_path.exists():
+                if snapshot_path.read_text() != value:
+                    snapshot_path.write_text(value)
+                    self._updated_snapshots.append(snapshot_name)
+            else:
+                snapshot_path.write_text(value)
+                self._created_snapshots.append(snapshot_name)
         else:
-            raise AssertionError("Snapshot '{}' doesn't exist in '{}'.\nRun pytest with --snapshot-update to create it.".format(snapshot_name, self.snapshot_dir))
+            if snapshot_path.exists():
+                expected_value = snapshot_path.read_text()
+                assert expected_value == value
+            else:
+                raise AssertionError("Snapshot '{}' doesn't exist in '{}'.\nRun pytest with --snapshot-update to create it.".format(snapshot_name, self.snapshot_dir))
--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -7,11 +7,12 @@ def basic_case_dir(testdir):
     case_dir = testdir.mkdir('case_dir')
     case_dir.join('snapshot1.txt').write_text(u'the value of snapshot1.txt', 'ascii')
 
+
 def test_assert_match_success(testdir, basic_case_dir):
     testdir.makepyfile("""
         def test_sth(snapshot):
             snapshot.snapshot_dir = 'case_dir'
-            snapshot.assert_match('the value of snapshot1.txt', 'snapshot1.txt')
+            snapshot.assert_match(u'the value of snapshot1.txt', 'snapshot1.txt')
     """)
 
     result = testdir.runpytest('-v')
@@ -25,7 +26,7 @@ def test_assert_match_failure(testdir, basic_case_dir):
     testdir.makepyfile("""
         def test_sth(snapshot):
             snapshot.snapshot_dir = 'case_dir'
-            snapshot.assert_match('the INCORRECT value of snapshot1.txt', 'snapshot1.txt')
+            snapshot.assert_match(u'the INCORRECT value of snapshot1.txt', 'snapshot1.txt')
     """)
 
     result = testdir.runpytest('-v')
@@ -44,7 +45,7 @@ def test_assert_match_missing_snapshot(testdir, basic_case_dir):
     testdir.makepyfile("""
         def test_sth(snapshot):
             snapshot.snapshot_dir = 'case_dir'
-            snapshot.assert_match('something', 'snapshot_that_doesnt_exist.txt')
+            snapshot.assert_match(u'something', 'snapshot_that_doesnt_exist.txt')
     """)
 
     result = testdir.runpytest('-v')
@@ -52,6 +53,54 @@ def test_assert_match_missing_snapshot(testdir, basic_case_dir):
         '*::test_sth FAILED*',
         "E* AssertionError: Snapshot 'snapshot_that_doesnt_exist.txt' doesn't exist in 'case_dir'.",
         'E* Run pytest with --snapshot-update to create it.',
+    ])
+    assert result.ret == 1
+
+
+def test_update_existing_snapshot_no_change(testdir, basic_case_dir):
+    testdir.makepyfile("""
+        def test_sth(snapshot):
+            snapshot.snapshot_dir = 'case_dir'
+            snapshot.assert_match(u'the value of snapshot1.txt', 'snapshot1.txt')
+    """)
+
+    result = testdir.runpytest('-v', '--snapshot-update')
+    result.stdout.fnmatch_lines([
+        '*::test_sth PASSED*',
+    ])
+    assert result.ret == 0
+
+
+def test_update_existing_snapshot(testdir, basic_case_dir):
+    testdir.makepyfile("""
+        def test_sth(snapshot):
+            snapshot.snapshot_dir = 'case_dir'
+            snapshot.assert_match(u'the NEW value of snapshot1.txt', 'snapshot1.txt')
+    """)
+
+    result = testdir.runpytest('-v', '--snapshot-update')
+    result.stdout.fnmatch_lines([
+        '*::test_sth PASSED*',
+        '*::test_sth ERROR*',
+        "E* AssertionError: The following snapshots were updated in 'case_dir':",
+        'E*   snapshot1.txt'
+    ])
+    assert result.ret == 1
+
+
+def test_create_new_snapshot(testdir, basic_case_dir):
+    testdir.makepyfile("""
+        def test_sth(snapshot):
+            snapshot.snapshot_dir = 'case_dir'
+            snapshot.assert_match(u'the NEW value of new_snapshot1.txt', 'new_snapshot1.txt')
+    """)
+
+    result = testdir.runpytest('-v', '--snapshot-update')
+    result.stdout.fnmatch_lines([
+        '*::test_sth PASSED*',
+        '*::test_sth ERROR*',
+        "E* AssertionError: The following snapshots were created in 'case_dir':",
+        'E*   new_snapshot1.txt'
     ])
     assert result.ret == 1
 
