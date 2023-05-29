7fc8b5f064776f9bea70957762631bff3f7dc0c6
Dan <DanOfTheRoses@gmail.com>
2023-05-29 12:57:30 -0500

    Simplify version
    Change slightly how version is populated.
    Increases test coverage to 100% at https://app.codecov.io/gh/joseph-roitman/pytest-snapshot

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup
 setup(
     name='pytest-snapshot',  # This line is only needed to make the Github dependency graph work
     packages=['pytest_snapshot'],  # This line is only needed to make the Github dependency graph work
-    use_scm_version={"write_to": "pytest_snapshot/_version.py"},
+    use_scm_version=True,
 )
